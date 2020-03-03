8bd29aa4b315326e467685d84e42f7f0d78f071b
tux116 <test157@qq.com>
2020-03-03 22:57:15 +0800

    v0.1

---
+++ import-package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> openwrt/feeds.conf.default
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git openwrt/package/luci-theme-atmaterial
+git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/luci-theme-argon
+git clone https://github.com/Myoko/openwrt-packages-theme.git
+mv openwrt-packages-theme/luci-theme-argon-dark-new openwrt/package/
+mv openwrt-packages-theme/luci-theme-atmaterial openwrt/package/
