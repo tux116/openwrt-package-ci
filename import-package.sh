b2fce17693d8c582d6ead4401f2179981d824c88
tux116 <test157@qq.com>
2020-03-03 23:30:19 +0800

    Update import-package.sh

--- import-package.sh
+++ import-package.sh
@@ -4,4 +4,4 @@ git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git openwrt/p
 git clone https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/luci-theme-argon
 git clone https://github.com/Myoko/openwrt-packages-theme.git
 mv openwrt-packages-theme/luci-theme-argon-dark-new openwrt/package/
-mv openwrt-packages-theme/luci-theme-atmaterial openwrt/package/
+# mv openwrt-packages-theme/luci-theme-atmaterial openwrt/package/
